e33a53facbebb0c3985c9a4a4cc30686fe6a31ae
mengdi he <1043881240@qq.com>
2022-04-05 21:36:31 +0800

    wind up

--- scripts/debug_compress.sh
+++ scripts/debug_compress.sh
@@ -5,5 +5,5 @@ JS_PATH=/home/hmd/acapp/game/static/js/
 JS_PATH_DIST=${JS_PATH}dist/
 JS_PATH_SRC=${JS_PATH}src/
 
-find $JS_PATH_SRC -type f -name *.js | sort | xargs cat | terser -c -m > ${JS_PATH_DIST}game.js
+find $JS_PATH_SRC -type f -name *.js | sort | xargs cat > ${JS_PATH_DIST}game.js
 cd $PROJECT_PATH && echo yes | python3 manage.py collectstatic
